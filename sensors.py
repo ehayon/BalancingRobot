ba063c09e39084fc5cd89dcc4dcfef92dfd295af
Ethan Hayon <ehayon@gmail.com>
2014-05-05 18:12:38 -0400

    code to intialize and read adxl345 accelerometer

--- sensors.py
+++ sensors.py
@@ -1,5 +1,14 @@
+import time
+from adxl345 import ADXL345
+
 class Sensors:
 
   def init(self):
     print "Initializing ADXL345 Accelerometer"
-    
+    adxl345 = ADXL345()
+    print "ADXL345 Initialized"
+    while True:
+      print adxl345.read()
+
+      time.sleep(0.1)
+
