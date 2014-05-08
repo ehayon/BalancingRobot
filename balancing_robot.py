d8b780e0de397f26fca46e258c71f8b1d34f7576
Ethan Hayon <ehayon@gmail.com>
2014-05-08 11:12:28 -0400

    PID control loop for balancing robot

--- balancing_robot.py
+++ balancing_robot.py
@@ -9,6 +9,14 @@ class BalancingRobot:
 
   calibrated_accel = 0.0
 
+  INTEGRAL_SAMPLES = 100
+
+  motor_output = 0.0
+
+  Kp = 0.2
+  Ki = 0.05
+  Kd = 0.09
+
   def init(self):
     print "Initializing Balancing Robot Code"
   
@@ -23,10 +31,10 @@ class BalancingRobot:
       print "Calibration step: %d (avg=%0.2f)" % (i, calibrated_sum / (i+1.0))
       calibrated_sum = calibrated_sum + globals.ACCEL[1]
       time.sleep(0.05)
-    calibrated_accel = calibrated_sum / 100.0
+    self.calibrated_accel = calibrated_sum / 100.0
 
     # calibration complete
-    print "Calibrated accelerometer value is %0.2f" % (calibrated_accel)
+    print "Calibrated accelerometer value is %0.2f" % (self.calibrated_accel)
 
 
 
@@ -36,15 +44,48 @@ class BalancingRobot:
 
     print "Attemptimg to balance the robot"
 
-    time.sleep(0.05)
+    print "PID gains: Kp = %0.2f\tKi = %0.2f\tKd=%0.2f" % (self.Kp, self.Ki, self.Kd)
+
+    count = 0
+    integral_sum = 0
+    prev_error = 0.0
+    error = 0.0
+
+    while True:
+      count = count % self.INTEGRAL_SAMPLES
+      if count == 0:
+        integral_sum = 0
+
+      accel_desired = self.calibrated_accel
+
+      prev_error = error
+      error = globals.ACCEL[1] - accel_desired
+
+      integral_sum = integral_sum + error
+      derivative_error = prev_error - error
+
+      pid = (self.Kp * error) + (self.Ki * (integral_sum / self.INTEGRAL_SAMPLES)) + (self.Kd * derivative_error)
+
+      motor_output = pid
+     
+      if motor_output < -100.0: motor_output = -100.0
+      if motor_output > 100.0: motor_output = 100.0
+
+      #print "PID corrective value = %0.2f" % (pid)
+      print "Output to motor = %0.2f" % (motor_output)
+      
+      count = count + 1
+      time.sleep(0.05)    
 
 
 if __name__ == "__main__":
+  # initialize the sensor reading thread
   sensors = Sensors()
   st = threading.Thread(target=sensors.init)
   st.daemon = True
   st.start()
 
+  # initialize the balancing robot thread
   balancing_robot = BalancingRobot()
   bt = threading.Thread(target=balancing_robot.run)
   bt.daemon = True
